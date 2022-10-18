ed43e676e076fe2985e7a81a278dfaaa979ea4a5
Seraph 天 <seraph776@gmail.com>
2022-10-18 10:59:05 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,13 +6,9 @@ contact: admin@codecrypt76.com
 project: IMDB Top 250
 """
 
-import logging
+
 import sqlite3
 import app_functions as app
-
-logging.basicConfig(filename='output.log',
-                    format='%(asctime)s: %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, encoding='utf8')
 
 
 def main():
@@ -21,18 +17,11 @@ def main():
 
     app.create_table(conn)
     soup = app.get_page(URL)
-    table_body = soup.find('tbody', attrs={'class': 'lister-list'}).find_all('tr')
-    for rank, movie in enumerate(table_body, start=1):
-        title = movie.find('td', attrs={'class': 'titleColumn'}).a.text
-        release_date = movie.find('span', attrs={'class': 'secondaryInfo'}).text.strip("()")
-        rating = float(movie.find('td', attrs={'class': 'ratingColumn imdbRating'}).strong.text)
-        app.save_data(conn, (rank, title, release_date, rating))
-        logging.info(f'Saving {rank}, {title}, {release_date}, {rating} to database.')
+    scraped_data = app.scrape_site(soup)
+    app.save_scraped_data(conn, scraped_data)
 
+    # View database
     app.view_database(conn)
-
-    # app.display_by_rating(conn, 9)
-    # app.display_by_rating(conn, 8)
 
 
 if __name__ == '__main__':
